e45b672a1b68445310c3820b14456b275f2ef26c
Matteo Pieroni <matteopieroni6@gmail.com>
2020-01-16 22:30:58 +0000

    Added loading bar

---
+++ src/components/articles-home/index.ts
@@ -0,0 +1,1 @@
+export { ArticlesHome } from './articles-home';
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,16 @@ import App from 'next/app';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 import withData from '../apollo-config';
+import { LoadingBar } from '../components/loading-bar';
 
 class MyApp extends App {
 	render() {
 		const { Component, pageProps, apollo } = this.props;
+
 		return (
 			<ApolloProvider client={apollo}>
 				<Component {...pageProps} />
+				<LoadingBar />
 			</ApolloProvider>
 		);
 	}
